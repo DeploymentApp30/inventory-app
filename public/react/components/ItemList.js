19dadf3297b2bebdc5f22d6d07e24c31644a6ed5
Eliana_Lugo <Ellelugo91@gmail.com>
2023-06-12 22:49:34 -0400

    ended promise chain with catch method

--- public/react/components/ItemList.js
+++ public/react/components/ItemList.js
@@ -48,43 +48,43 @@ export const ItemsList = ({items, setItems}) => {
 		});
 		const data = await response.json();
 	}
-	return <div >
-		{oneItem ? 
-		<div class="container">
-		<div class="row justify-content-md-center">
-		  <div class="col-5" >
-			<div id='description'>
-				<h2>Category: {oneItem.category}</h2>
-				<h6>{oneItem.description}</h6>
-				<h3>${oneItem.price}</h3>
-				<div>
-				<Button onClick={()=>{addToCart(oneItem.id)}}id="addToCartButton">Add to Cart</Button>
-				<UpdateForm oneItemid = {oneItem.id}/>
+	return (
+		<div>
+		  {oneItem ? (
+			<div class="container">
+			  <div class="row justify-content-md-center">
+				<div class="col-5">
+				  <div id='description'>
+					<h2>Category: {oneItem.category}</h2>
+					<h6>{oneItem.description}</h6>
+					<h3>${oneItem.price}</h3>
+					<div>
+					  <Button onClick={() => addToCart(oneItem.id)} id="addToCartButton">
+						Add to Cart
+					  </Button>
+					  <UpdateForm oneItemid={oneItem.id} />
+					</div>
+				  </div>
+				  <Stack gap={2}>
+					<Button onClick={() => goBack()} style={{ width:'200px'}}>Go Back</Button>
+					<Button onClick={() => deleteItem(oneItem.id)} style={{ width:'200px'}}>Delete</Button>
+				  </Stack>
 				</div>
-
+				<div class="col" id="oneItemImg">
+				  <img src={oneItem.image} />
+				</div>
+			  </div>
 			</div>
-			<Stack gap={2}>
-			<Button onClick={() => goBack()} style={{ width:'200px'}}>Go Back</Button>
-			<Button onClick={() => deleteItem(oneItem.id)} style={{ width:'200px'}}>Delete</Button>
-        	</Stack>
-		  </div>
-		  <div class="col" id="oneItemImg">
-			<img src={oneItem.image} />
-		  </div>
-		</div>
-		</div>
-		:
-		<div className='d-flex flex-wrap justify-content-between gap-2'>
-			<div className='w-100 mb-2'>
-			<AddItemModal/>
+		  ) : (
+			<div className='d-flex flex-wrap justify-content-between gap-2'>
+			  <div className='w-100 mb-2'>
+				<AddItemModal/>
+			  </div>
+			  {items.map((item, idx) => {
+				return <Item item={item} key={idx} clickThis={getItemsId} />;
+			  })}
 			</div>
-			{
-			items.map((item, idx) => {
-			return <Item item={item} key={idx} clickThis={getItemsId} />
-		})
-		}
+		  )}
 		</div>
-		
-		}
-	</div>
+	  );	  
 } 

2ea8325393a8dcffcce506e0561e57c03c32b37f
Eliana_Lugo <Ellelugo91@gmail.com>
2023-06-12 23:06:47 -0400

    added try-catch block

--- server/seed.js
+++ server/seed.js
@@ -1,25 +1,20 @@
-const {sauces, items, cart} = require('./seedData.js');
-
-const {sequelize} = require('./db');
-const {Sauce,Items, Cart } = require('./models');
-
+const { sauces, items, cart } = require('./seedData.js');
+const { sequelize } = require('./db');
+const { Sauce, Items, Cart } = require('./models');
 
 const seed = async () => {
+  try {
+    await sequelize.sync({ force: true });
 
-    try {
-        // drop and recreate tables per model definitions
-        await sequelize.sync({ force: true });
-    
-        // insert data
-        await Promise.all(sauces.map(sauce => Sauce.create(sauce)));
-        await Promise.all(items.map(item => Items.create(item)))
-        await Promise.all(cart.map(cart => Cart.create(cart)))
+    await Promise.all(sauces.map(sauce => Sauce.create(sauce)));
+    await Promise.all(items.map(item => Items.create(item)));
+    await Promise.all(cart.map(cartItem => Cart.create(cartItem)));
 
-        console.log("db populated!");
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
-}
+    console.log("Database seeded successfully!");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+    process.exit(1); // Terminate the process with a non-zero exit code to indicate failure
+  }
+};
 
 seed();
